2b349d222b6a03ee1dcac8fb29eeed1a0110585d
madhavsahi <madhavsahi16@gmail.com>
2023-08-22 21:11:37 +0530

    added tailwind and did minor modifications

--- youtube-reactjs/src/App.js
+++ youtube-reactjs/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+//we don;t require import react now...it;s done in index.js
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <p>HIlll</p>
+    </>
+  )
 }
 
 export default App;
